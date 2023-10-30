e250f6d2d3c80a9d3c03bffe552e0d478a62cef5
NakibingeSteven <88508645+NakibingeSteven@users.noreply.github.com>
2023-10-30 15:10:25 +0300

    helloWorld

---
+++ hello.php
@@ -0,0 +1,11 @@
+<?php
+$first_name = "Nakibinge";
+$second_name ="Steven";
+echo "Helo World ,I am \n". $first_name . $second_name;
+?>
+
+describe the perfect idel suprvisor/teml ead for applied sciencs
+ strengths
+ techincal strenght
+
+ implemetn and enfore document manegemtn systems
